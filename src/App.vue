f4b09e8176ce8f310726bb6eb1ee025cea167557
MD <mdv@sncard.ru>
2025-04-23 15:07:32 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    hello world
-  </main>
+  <main>test git</main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
