e6af7ea75888197fa785081a7938ad3eba778773
MD <mdv@sncard.ru>
2025-04-23 18:18:00 +0700

    init map

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, useTemplateRef } from "vue";
+
+declare var ymaps: any
+const mapElement = useTemplateRef<any>('map')
+let mapObject: any
+const init = (): void => {
+
+  mapObject = new ymaps.Map(mapElement.value, {
+    // При инициализации карты обязательно нужно указать
+    // её центр и коэффициент масштабирования.
+    center: [55.76, 37.64], // Москва
+    zoom: 10
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+}
+
+ymaps.ready(init)
+</script>
 
 <template>
-  <main>test git</main>
+  <div id="map" ref="map" class="map-container h-full"></div>
 </template>
 
 <style scoped></style>
