2e5ea689a2c4c78de8e6713825d10a720ac3ce99
MD <mdv@sncard.ru>
2025-04-24 16:02:37 +0700

    pwa start

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
@@ -12,26 +12,41 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ["favicon.ico","logo-pwa.png"],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "logo-pwa144.png"],
       manifest: {
-        name: 'My Vue PWA',
-        short_name: 'VuePWA',
-        description: 'My Vue 3 Progressive Web App',
-        theme_color: '#ffffff',
+        name: "My Vue PWA",
+        short_name: "VuePWA",
+        description: "My Vue 3 Progressive Web App",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'logo-pwa.png',
-            sizes: '53x43',
-            type: 'image/png'
+            src: "logo-pwa144.png",
+            sizes: "144x144",
+            type: "image/png"
+          }
+        ],
+        screenshots: [
+          {
+            src: "image1.png",
+            sizes: "640x579",
+            type: "image/png",
+            form_factor: "wide",
+            label: "Application"
+          },
+          {
+            src: "image1.png",
+            sizes: "640x579",
+            type: "image/png"
           }
         ]
       }
+
     })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 });
