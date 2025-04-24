7368cd288e4059f4542b0aa91f125189fdb40471
MD <mdv@sncard.ru>
2025-04-24 15:43:36 +0700

    pwa start

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,23 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ["favicon.ico","logo-pwa.png"],
+      manifest: {
+        name: 'My Vue PWA',
+        short_name: 'VuePWA',
+        description: 'My Vue 3 Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-pwa.png',
+            sizes: '53x43',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
